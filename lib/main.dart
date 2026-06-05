6bc16867f76f9c0d8e266bf5db637f339563f24b
Carlos Ospina <caospinap@gmail.com>
2026-06-05 17:16:52 -0500

    start project

---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<String> tasks = [];
+
+  void createTask(String title) {
+    if (title.isEmpty) {
+      throw Exception('Título requerido');
+    }
+
+    tasks.add(title);
+
+    // Guardar localmente
+    SharedPreferences.getInstance().then((preferences) {
+      preferences.setStringList('tasks', tasks);
+    });
+
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Tasks')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          createTask('Tarea ${tasks.length + 1}');
+        },
+        child: const Icon(Icons.add),
+      ),
+      body: ListView.builder(
+        itemCount: tasks.length,
+        itemBuilder: (_, index) {
+          return ListTile(title: Text(tasks[index]));
+        },
+      ),
+    );
+  }
+}
